a5f9f23720f73f4bed3bd0f5a0fd0ba6e91270cf
Michael Sh <michael.sh@gmx.at>
2025-10-16 22:30:54 +0200

    enhanced login component with username and password field

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthGoogleService } from '../services/auth-google.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const MODULES: any[] = [
   MatButtonModule,
@@ -11,6 +15,10 @@ const MODULES: any[] = [
   MatFormFieldModule,
   FormsModule,
   ReactiveFormsModule,
+  CommonModule,
+  MatCardModule,
+  MatInputModule,
+  MatProgressSpinnerModule
 ];
 
 @Component({
@@ -24,7 +32,35 @@ const MODULES: any[] = [
 export class Login {
   private authService = inject(AuthGoogleService);
 
+  loginForm!: FormGroup;
+
+  hide = true;
+  loading = false;
+  errorMessage = '';
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
   signInWithGoogle() {
     this.authService.login();
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Please fill in all required fields correctly.';
+      return;
+    }
+
+    this.errorMessage = '';
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+      alert('✅ Login successful!');
+    }, 1200);
   }
 }
